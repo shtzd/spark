0c52e45c94d388e878e800d6c91603e030658652
shtzd <tzdkkk@163.com>
2020-11-25 20:29:06 +0800

    测试pull

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,7 @@
 public class HelloWorld {
-
+    public static void main(String[] args) {
+        System.out.println("测试clone");
+        System.out.println("测试push");
+        System.out.println("测试pull");
+    }
 }
